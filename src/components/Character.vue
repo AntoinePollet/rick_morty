94185a1bb6bf4530224f35a99e4d6a909854d59f
AntoinePollet <pollet.antoine.alexis@gmail.com>
2021-11-02 10:21:44 +0100

    adding character's origin

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,47 +1,44 @@
 <template>
-  <div class="m-3 bg-red-200">
+  <div v-if="character" class="m-3 bg-red-200">
     <div class="flex flex-col md:flex-row">
       <div class="py-5">
-        <img class="rounded-full object-contain" width="175" height="175" :src="character.image" :alt="character.name"/>
+        <img class="rounded-full" height="175" width="175" :src="character.image" :alt="character.name"/>
       </div>
-      <div class="py-5">
-        <p>
-          Hey you, I am {{character.name}}, I come from
-          {{character.location.type}} {{character.location.name}}
+      <div class="py-5 flex items-center flex-col">
+        <p>This is <span class="highlight">{{character?.name}}</span> and I am located on</p>
+        <p class="highlight">
+          {{character?.location?.type}} {{character?.location?.name}}
         </p>
       </div>
     </div>
     <div>
       <div class="text-left ml-4 my-4">
-        <p class="cursor-pointer" @click="showLocation = !showLocation">
-          Where can I find this character ?
-          <font-awesome-icon :icon="['fas', isTrue(showLocation)]"/>
-        </p>
-        <div v-if="showLocation" class="ml-4">
-          <p>Comes from {{character.location.type}} {{character.location.name}}</p>
-          <div>
-            <p class="cursor-pointer" @click="showResidents = !showResidents">
-              See every residents
-              <font-awesome-icon :icon="['fas', isTrue(showResidents)]"/>
-            </p>
-            <div v-if="showResidents">
-              <template v-for="resident in character.location.residents" :key="resident.id">
-                <img :src="resident.image" class="rounded-full w-1/5 p-2" :alt="resident.name"/>
-              </template>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="text-left ml-4 my-4">
         <p class="cursor-pointer" @click="showOrigin = !showOrigin">
-          Where is he from ?
+          Where is {{ character.name }} originally from ?
           <font-awesome-icon :icon="['fas', isTrue(showOrigin)]"/>
         </p>
         <div v-if="showOrigin" class="ml-4">
-          <p>{{character.origin}}</p>
+          <p>{{knownOrigin(character.origin)}}</p>
         </div>
       </div>
+      <div class="text-left ml-4 my-4">
+        <div>
+          <p class="cursor-pointer" @click="showResidents = !showResidents">
+            See every residents of {{character?.location?.name}}
+            <font-awesome-icon :icon="['fas', isTrue(showResidents)]"/>
+          </p>
+          <div v-if="showResidents">
+            <template v-for="resident in character?.location?.residents" :key="resident.id">
+              <img :src="resident.image" class="rounded-full w-1/5 p-2" :alt="resident.name"/>
+            </template>
+          </div>
+        </div>
+      </div>
+
     </div>
+  </div>
+  <div v-else>
+    Loading...
   </div>
 </template>
 
@@ -65,11 +62,16 @@ export default {
   methods: {
     isTrue(bool) {
       return bool ? 'angle-down' : 'angle-up'
+    },
+    knownOrigin(origin) {
+      return origin.name !== 'unknown' ? `${origin.type} ${origin.name} ` : 'unknown origin'
     }
   }
 }
 </script>
 
 <style lang="scss">
-
+.highlight {
+  @apply bg-gray-100 py-1 my-2 px-2 rounded
+}
 </style>
